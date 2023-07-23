0f01a2df94823c6b142e8174a5754efea1b24153
joaoroquesh <joaoroquesh@gmail.com>
2023-07-23 18:12:49 -0300

    Ajuste menu e Gtag

---
+++ modules/fixed/head.js
@@ -0,0 +1,28 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7SD3F94ZD7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7SD3F94ZD7');
+</script>
+
+<meta name="msapplication-TileColor" content="#333">
+<meta name="theme-color" content="#333">
+
+<meta property="og:site_name" content="João Roque Design">
+<meta name="site_name" content="João Roque Design">
+<meta property="og:title" content="João Roque Design" />
+<meta name="title" content="João Roque Design" />
+<meta property="og:description" content="Estratégia Visual">
+<meta name="description" content="Estratégia Visual">
+<meta property="og:url" content="https://www.joaoroquedesign.com/" />
+<meta name="url" content="https://www.joaoroquedesign.com/" />
+<meta property="og:image" itemprop="image" content="images/logo.svg" />
+<meta name="image" itemprop="image" content="images/logo.svg" />
+<meta property="og:image:type" content="image/svg">
+<meta name="image:type" content="image/svg">
+<meta property="og:image:width" content="600">
+<meta property="og:image:height" content="300">
+<meta property="og:type" content="website" />
